0c0d1acb075af38afd89dfcf4488d257e874382f
PoliGold <andrea.polidoro@outlook.it>
2018-05-21 16:53:08 +0200

    adding proccessing image

--- address/js/address-custom.js
+++ address/js/address-custom.js
@@ -1,10 +1,11 @@
+
 $(document).ready(function() {
   $('#address-table').dynatable({
     dataset: {
       ajax: true,
-      ajaxUrl: 'http://10.3.56.48:8080/rmdw-1.0.0-beta/extrarest/v1.0.0/rmdw/assets/UserEntity',
-      ajaxOnLoad: true,
-      records: []
+      ajaxUrl: 'http://localhost:8080/rmdw-1.0.0-beta/extrarest/v1.0.0/rmdw/assets/UserEntity',
+      ajaxonLoad: true,
+      records: [],
     },
     features: {
       paginate: true,
@@ -14,6 +15,9 @@ $(document).ready(function() {
       queryRecordCount: 'totalElements',
       records: 'elements',
       perPage: 'size'
+    },
+    inputs: {
+      processingText: '<div class="processing"><img src="img/loading.gif" /></div>'
     }
   });
 });
